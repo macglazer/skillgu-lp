bae26218343f914ee5c05f563f29d33eb673cbaf
mkowack <kowackag@gmail.com>
2024-07-26 23:37:13 +0200

    Fix layout Topbar

--- src/app/_components/topbar/Topbar.tsx
+++ src/app/_components/topbar/Topbar.tsx
@@ -10,21 +10,23 @@ import Logo from '@/app/ui/icons/Logo'
 export const Topbar = () => {
   const [isShown, setIsShown] = useState(false)
   return (
-    <div className="fixed left-0 right-0 top-0 m-auto flex max-w-[1304px] items-center justify-between border-b border-base200 bg-base000 px-5 py-4">
-      <Link
-        className="flex items-center justify-center"
-        href=""
-        aria-label="Go to Home Page"
-      >
-        <Logo />
-      </Link>
-      <Navigation className="hidden lg:flex" />
-      {isShown ? (
-        <div className="fixed bottom-0 left-0 right-0 top-16 border-t border-base200 bg-base000 px-5 py-6 lg:hidden">
-          <Navigation className="h-full" onClick={() => setIsShown(false)} />
-        </div>
-      ) : null}
-      <MobileMenuButton onClick={() => setIsShown((state) => !state)} />
+    <div className="fixed left-0 right-0 top-0 border-b border-base200 bg-base000">
+      <div className="m-auto flex max-w-[1304px] items-center justify-between px-5 py-4">
+        <Link
+          className="flex items-center justify-center"
+          href=""
+          aria-label="Go to Home Page"
+        >
+          <Logo />
+        </Link>
+        <Navigation className="hidden lg:flex" />
+        {isShown ? (
+          <div className="fixed bottom-0 left-0 right-0 top-16 border-t border-base200 bg-base000 px-5 py-6 lg:hidden">
+            <Navigation className="h-full" onClick={() => setIsShown(false)} />
+          </div>
+        ) : null}
+        <MobileMenuButton onClick={() => setIsShown((state) => !state)} />
+      </div>
     </div>
   )
 }
