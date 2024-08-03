193c6c879d0e190f3057d9ac7b19490a9acd7a4b
mkowack <kowackag@gmail.com>
2024-08-03 20:09:37 +0200

    Feat Add Alcatra font

--- src/app/_components/hero-banner/HeroBanner.tsx
+++ src/app/_components/hero-banner/HeroBanner.tsx
@@ -1,13 +1,23 @@
 import { Paths } from '@/app/paths'
 import Link from 'next/link'
+import clx from 'classnames'
+
+import { alcatra } from '@/app/fonts'
 
 export const HeroBanner = () => {
   return (
     <section className="mx m-auto my-24 max-w-2xl px-5">
       <h3 className="hidden">Skillgu home page</h3>
-      <p className="mb-8 text-center text-32px font-bold leading-[48px] tracking-[-1px] text-primary lg:text-48px lg:leading-72px lg:tracking-[-2px]">
+      <p className="mb-8 w-full text-center text-32px font-bold leading-[48px] tracking-[-1px] text-primary lg:text-48px lg:leading-72px lg:tracking-[-2px]">
         Osiągaj swoje cele szybciej z{' '}
-        <span className="font-artist italic text-secondary">1:1</span>{' '}
+        <span
+          className={clx(
+            alcatra.className,
+            'font-semibold italic tracking-wide text-secondary',
+          )}
+        >
+          1:1
+        </span>{' '}
         mentoringiem
       </p>
       <Link
