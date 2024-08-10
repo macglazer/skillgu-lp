5b47927b5c229a377d8717f1e893f1e8ea8b5721
mkowack <kowackag@gmail.com>
2024-08-10 23:49:18 +0200

    feat: add policy and terms

---
+++ src/app/(static-pages)/[filename]/page.tsx
@@ -0,0 +1,30 @@
+import { notFound } from 'next/navigation'
+import { type ComponentType } from 'react'
+
+export default async function StaticPage({
+  params,
+}: {
+  params: { filename: string }
+}) {
+  const Content = await import(`./${params.filename}.mdx`).then(
+    (m: { default: ComponentType }) => m.default,
+    () => notFound(),
+  )
+
+  const title = {
+    policy: 'Polityka prywatności',
+    terms: 'Regulamin',
+  }
+
+  console.log(params.filename)
+  return (
+    <article className="m-auto flex max-w-xl flex-col gap-10 px-5 py-[44px] text-primary md:max-w-fit lg:max-w-[1304px] lg:flex-row">
+      <h3 className="text-[28px] font-bold tracking-[-1px] lg:text-5xl lg:leading-[72px] lg:tracking-[-2px]">
+        {params.filename === 'policy' || params.filename === 'terms'
+          ? title[params.filename]
+          : null}
+      </h3>
+      <Content />
+    </article>
+  )
+}
