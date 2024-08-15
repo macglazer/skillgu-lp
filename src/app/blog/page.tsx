d5bdeab67262f610211559b82eedaf734607e8da
mkowack <kowackag@gmail.com>
2024-08-15 22:38:58 +0200

    refactor(Blog): remove unused imports

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,4 @@
-import { TeamMemberCard } from '../_components/TeamMemberCard'
-import { fetchAllMembers, Member } from '@/services/fetchAllMembers'
-
 const Blog = async () => {
-
   return (
     <section className="m-auto flex w-full max-w-xl flex-col gap-10 p-[44px] px-5 md:max-w-fit lg:max-w-[1304px]">
       <h3 className="semibold text-[28px] font-bold leading-[42px] lg:text-[48px] lg:leading-[72px]">
