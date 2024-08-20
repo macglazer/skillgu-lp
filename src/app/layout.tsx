56e50532a9dc7ab2f0ef4762c18853f093e6b201
mkowack <kowackag@gmail.com>
2024-08-20 12:29:48 +0200

    feat: integrate Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { GoogleTagManager } from '@next/third-parties/google'
+import { Montserrat } from 'next/font/google'
+import './globals.css'
 
-const montserrat = Montserrat({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Skillgu",
-  description: "Miejsce, gdzie znajdziesz swojego mentora. ",
-};
+  title: 'Skillgu',
+  description: 'Miejsce, gdzie znajdziesz swojego mentora. ',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
+
   return (
-    <html lang="en">
+    <html lang="pl">
       <body className={montserrat.className}>{children}</body>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GA_ID as string} />
     </html>
-  );
+  )
 }
