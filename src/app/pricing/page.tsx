228e48c2de5bab10d696701c2461ab50eb36b9f0
mkowack <kowackag@gmail.com>
2024-09-03 00:05:01 +0200

    style: title pricing page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -3,7 +3,7 @@ import { PricingPlan } from '@/ui/PricingPlan'
 export default async function Pricing() {
   return (
     <section className="m-auto flex w-full max-w-xl flex-col gap-10 p-[44px] px-5 md:max-w-fit lg:max-w-[1304px]">
-      <h3 className="mb-10 text-center text-[36px] font-bold leading-[1.2] tracking-normal text-primary lg:text-5xl lg:leading-[72px]">
+      <h3 className="text-center text-[36px] font-bold leading-[1.2] tracking-normal text-primary lg:text-5xl lg:leading-[72px]">
         Cennik
       </h3>
       <div className="flex flex-col justify-center gap-8 md:flex-row md:flex-wrap">
