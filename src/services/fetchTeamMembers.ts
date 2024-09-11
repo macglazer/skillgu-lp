7acf98f197f932548264259d178e46f148780402
maciejmaciek <hello@maciejglazer.com>
2024-09-11 14:13:42 +0200

    update

--- src/services/fetchTeamMembers.ts
+++ src/services/fetchTeamMembers.ts
@@ -37,7 +37,7 @@ export const fetchTeamMembers = async (): Promise<ResponseData> => {
                     'Pomysłodawca, założyciel oraz jeden z programistów Skillgu. Od zawsze planowałem założyć własną firmę, a Skillgu jest realizacją tej wizji.' +
                     ' Jako członek zespołu programistycznego, aktywnie uczestniczę w rozwijaniu naszej platformy, łącząc technologię z moją wizją rozwoju kariery. ' +
                     'Głęboko wierzę, że praca z mentorami przyspiesza rozwój zawodowy, pomaga w pokonywaniu barier i odblokowywaniu potencjału na każdym etapie kariery.',
-                id: '2',
+                id: '1',
                 name: 'Maciej Glazer',
                 title: 'Founder Skillgu',
                 username: 'Maciekskillguru-1997887680',
@@ -76,33 +76,34 @@ export const fetchTeamMembers = async (): Promise<ResponseData> => {
             //     },
             // },
             {
-              avatar_url:
-                'https://res.cloudinary.com/dkclg8ppw/image/upload/v1726056411/MalgorzataKowacka_vxwhbu.jpg',
-              description:
-                'Małgorzata front-developer`ka, współtwórczyni skillgu oraz super koleżanka. Razem z Radosławem trzyma rękę na pulsie przy tworzeniu skillgu. Magłorzata również chętnie pomoże nowicjuszom wejść na rynek IT',
-              id: '1',
-              name: 'Małgorzata Kowacka',
-              title: 'Frontend React Developer',
-              username: 'gosia_kow',
-              socials: {
-                skillgu: 'https://www.skillgu.com/',
-                linkedin: 'https://www.linkedin.com/in/malgorzata-kowacka/',
-              },
+                avatar_url:
+                    'https://res.cloudinary.com/dkclg8ppw/image/upload/v1725969650/wdojtrnyn9olfr9xjjoa.jpg',
+                description:
+                    'Radosław współptwórca skillgu.com, świetny teammate oraz senior front-developer i team leader z 15 letnim stażem. W Skillgu współodpowiedzialny za front i super atmosferę. Radosław oprócz swoich usług jest również mentorem. Po więcej informacji zapraszamy na profil Radosława',
+                id: '4',
+                name: 'Radosław Żaczkiewicz',
+                title: 'Senior React Developer',
+                username: 'rado_ms',
+                socials: {
+                    skillgu: 'https://www.skillgu.com/',
+                    linkedin: 'https://www.linkedin.com/in/rados%C5%82aw-%C5%BCaczkiewicz-2534251bb/',
+                },
             },
             {
-              avatar_url:
-                'https://res.cloudinary.com/dkclg8ppw/image/upload/v1725969650/wdojtrnyn9olfr9xjjoa.jpg',
-              description:
-                'Radosław współptwórca skillgu.com, świetny teammate oraz senior front-developer i team leader z 15 letnim stażem. W Skillgu współodpowiedzialny za front i super atmosferę. Radosław oprócz swoich usług jest również mentorem. Po więcej informacji zapraszamy na profil Radosława',
-              id: '3',
-              name: 'Radosław Żaczkiewicz',
-              title: 'Senior React Developer',
-              username: 'rado_ms',
-              socials: {
-                skillgu: 'https://www.skillgu.com/',
-                linkedin: 'https://www.linkedin.com/in/rados%C5%82aw-%C5%BCaczkiewicz-2534251bb/',
-              },
+                avatar_url:
+                    'https://res.cloudinary.com/dkclg8ppw/image/upload/v1726056411/MalgorzataKowacka_vxwhbu.jpg',
+                description:
+                    'Małgorzata front-developer`ka, współtwórczyni skillgu oraz super koleżanka. Razem z Radosławem trzyma rękę na pulsie przy tworzeniu skillgu. Magłorzata również chętnie pomoże nowicjuszom wejść na rynek IT',
+                id: '3',
+                name: 'Małgorzata Kowacka',
+                title: 'Frontend React Developer',
+                username: 'gosia_kow',
+                socials: {
+                    skillgu: 'https://www.skillgu.com/',
+                    linkedin: 'https://www.linkedin.com/in/malgorzata-kowacka/',
+                },
             },
+
             // {
             //   avatar_url:
             //     'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
