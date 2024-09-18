fbc3455a52a687d71ca20ee3cb54549cac366fd8
mkowack <kowackag@gmail.com>
2024-09-18 10:53:18 +0200

    feat:new Hero section

--- src/app/_components/hero-banner/HeroBanner.tsx
+++ src/app/_components/hero-banner/HeroBanner.tsx
@@ -1,70 +1,83 @@
-'use client';
-
-import React, { Suspense, useState, useEffect } from 'react';
-import Link from 'next/link';
-import { Paths } from '@/app/paths';
+import React, { Suspense } from 'react'
+import Link from 'next/link'
+import { Paths } from '@/app/paths'
+import { Stripe } from '@/ui/icons/Stripe'
+import { TypingAnimation } from '@/ui/TypingAnimation'
 
 const HeroBannerVideo = React.lazy(() =>
-    import('./HeroBannerVideo').then((module) => ({
-        default: module.HeroBannerVideo,
-    }))
-);
+  import('./HeroBannerVideo').then((module) => ({
+    default: module.HeroBannerVideo,
+  })),
+)
 
 export const HeroBanner = () => {
-    const [text, setText] = useState('');
-    const [loopNum, setLoopNum] = useState(0);
+  const textArray = [
+    'IT',
+    'startupów',
+    'UX/UI',
+    'biznesu',
+    'marketingu',
+    'rozwoju zawodowego',
+    'strategii biznesowej',
+    'projektowania graficznego',
+    'zarządzania projektami',
+    'innowacji technologicznych',
+    'montażu video',
+    'social media',
+  ]
 
-    const textArray = [
-        'IT',
-        'startupów',
-        'UX/UI',
-        'biznesu',
-        'marketingu',
-        'rozwoju zawodowego',
-        'strategii biznesowej',
-        'projektowania graficznego',
-        'zarządzania projektami',
-        'innowacji technologicznych',
-        'montażu video',
-        'social media',
-    ];
+  return (
+    <section className="relative m-auto px-5 lg:max-w-[1440px]">
+      <div className="m-auto px-5 pt-88px">
+        <h3 className="hidden">Skillgu home page</h3>
 
-    useEffect(() => {
-        const handleTextChange = () => {
-            setText(textArray[loopNum % textArray.length]);
-            setLoopNum((prevLoopNum) => prevLoopNum + 1);
-        };
+        <div className="mb-88px flex flex-col md:flex-row w-full items-start justify-between gap-10 text-primary">
+          <div>
+            <p className="mb-8 w-full text-4xl font-bold leading-[56px] tracking-tight lg:text-56px lg:leading-[84px]">
+              <span className="text-4xl font-bold leading-[56px] tracking-tight text-primary lg:text-56px lg:leading-[84px]">
+                Znajdź swojego <br className="hidden md:block"/> mentora od:
+                <br />
+              </span>{' '}
+              <TypingAnimation
+                words={textArray}
+                typingSpeed={3}
+                deletingSpeed={9}
+                deletingDelay={1000}
+                className="mt-4 font-bold text-orange-500 transition-opacity duration-500 ease-in-out"
+                cursorClassName="border-red-300"
+              />
+            </p>
+            <Link
+              href={Paths.MENTORS}
+              className="link-primary-rounded inline-block w-[180px] py-4 text-14px"
+            >
+              Znajdź mentora
+            </Link>
+          </div>
+          <div className="w-full rounded-[20px] border border-base400 p-10 md:max-w-[50%] xl:max-w-lg">
+            <p className="mb-4 text-2xl font-bold tracking-tight lg:text-3xl">
+              Czym jest Skillgu?
+            </p>
+            <p className="mb-4 text-14px lg:text-base font-medium leading-6 lg:leading-[28.8px] text-base600">
+              Skillgu to platforma stworzona z myślą o tysiącach mentorów,
+              trenerów i osób, chcących wymieniać wiedzę oraz doświadczenia
+              online. Jesteśmy również uzupełnieniem dla kursów i szkoleń wideo.
+            </p>
+            <span className="lg:flex lg:gap-2 text-base font-semibold leading-[28.8px] text-base800">
+              Płatności obsługiwane przez {" "}
+              <Stripe className="inline"/>
+            </span>
+          </div>
+        </div>
 
-        const interval = setInterval(handleTextChange, 1500);
-
-        return () => clearInterval(interval);
-    }, [loopNum, textArray]);
-
-    return (
-        <section className="relative m-auto px-5 lg:max-w-[1440px]">
-            <div className="m-auto px-5 py-24">
-                <h3 className="hidden">Skillgu home page</h3>
-                <div className="flex flex-col justify-center items-center mb-8 w-full text-center text-32px font-bold leading-[48px] tracking-[-1px] text-primary lg:text-48px lg:leading-72px lg:tracking-[-2px]">
-                    <span className="font-semibold tracking-wide text-primary">
-                        Twój mentor od:
-                    </span>
-                    <span
-                        className="font-semibold tracking-wide text-orange-500 mt-4 transition-opacity duration-500 ease-in-out"
-                        style={{ opacity: text ? 1 : 0 }} // Zmienna przezroczystość
-                    >
-                        {text}
-                    </span>
-                </div>
-                <Link
-                    href={Paths.MENTORS}
-                    className="link-primary m-auto mb-12 block w-[180px] py-[14px] text-14px font-[600]"
-                >
-                    Znajdź mentora
-                </Link>
-                <Suspense fallback={<div className="h-[400px] md:h-[495px] lg:h-[645px] rounded-3xl border" />}>
-                    <HeroBannerVideo />
-                </Suspense>
-            </div>
-        </section>
-    );
-};
+        <Suspense
+          fallback={
+            <div className="h-[400px] rounded-3xl border md:h-[495px] lg:h-[645px]" />
+          }
+        >
+          <HeroBannerVideo />
+        </Suspense>
+      </div>
+    </section>
+  )
+}
