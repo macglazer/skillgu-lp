4e24cef3de8ac237321b303f1a95bf4e7cb405b0
mkowack <kowackag@gmail.com>
2024-09-19 09:37:33 +0200

    feat: mentee

--- src/app/mentee/page.tsx
+++ src/app/mentee/page.tsx
@@ -2,7 +2,7 @@ export default async function Mentee() {
   return (
     <section className="m-auto flex w-full max-w-xl flex-col gap-10 p-[44px] px-5 md:max-w-fit lg:max-w-[1304px]">
       <h3 className="text-center text-[36px] font-bold leading-[1.2] tracking-normal text-primary lg:text-5xl lg:leading-[72px]">
-        Dla mentee
+        Dla mentee page
       </h3>
       <div className="flex flex-col justify-center gap-8 md:flex-row md:flex-wrap"></div>
     </section>
