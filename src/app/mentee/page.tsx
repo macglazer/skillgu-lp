499a661587e38b82e87cb42e554edf16c743b709
maciejmaciek <hello@maciejglazer.com>
2024-09-23 08:07:34 +0200

    update

--- src/app/mentee/page.tsx
+++ src/app/mentee/page.tsx
@@ -26,9 +26,9 @@ export default async function MenteePage({ searchParams }: MenteePageType) {
               Benefity dla mentee
             </p>
             <p className="mb-5 w-[94%] text-14px font-medium leading-6 text-base600 lg:mb-8 lg:text-base lg:leading-[28.8px]">
-            Jeżeli utknąłeś w miejscu, kursy online to za mało, albo po prostu potrzebujesz bezpośredniej pomocy w postaci konsultacji, przygotować się do interview,
-              popracować nad trudnym zagadnieniem, przebranżowić się lub przegadać plan na własny startup, Skillgu to własnie to miejsce, gdzie możesz znaleść
-              doświadczonego trenera.
+              Jeśli Twoja kariera utknęła w miejscu, kursy online to za mało, albo po prostu potrzebujesz bezpośredniej pomocy w postaci konsultacji, przygotować się do interview,
+              popracować nad trudnym zagadnieniem, przebranżowić, lub przeanalizować plan na własny startup, Skillgu to własnie to miejsce, gdzie możesz znaleść
+              doświadczonego konsultanta.
             </p>
           </div>
           <div className="w-full md:w-[50%] lg:w-full">
