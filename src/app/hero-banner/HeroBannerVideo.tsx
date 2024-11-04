b6985f3d1f57532c7509abec17fb1390e62acdcc
Tomasz Rozpara <kowackag@gmail.com>
2024-11-04 21:26:10 +0100

    feat:change movie

--- src/app/hero-banner/HeroBannerVideo.tsx
+++ src/app/hero-banner/HeroBannerVideo.tsx
@@ -18,7 +18,7 @@ export const HeroBannerVideo = () => {
     <div className="m-auto w-full h-[400px] md:h-[495px] lg:h-[620px] max-w-[1120px] rounded-3xl md:px-14 md:pt-14 lg:px-20 lg:pt-20 bg-primary/5">
       <YouTube
         className={clx('h-full w-full md:rounded-t-3xl', styles.yt)}
-        videoId="YaBVqgA4VOU"
+        videoId="FeT8KWcWgLc"
         opts={opts}
       />
     </div>
