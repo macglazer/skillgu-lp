8e7fe9a9b15ee127bdc0e7ea1694c7e0708b18f0
maciejmaciek <hello@maciejglazer.com>
2024-12-07 17:24:53 +0100

    update categories url

---
+++ src/app/types/mentor.tsx
@@ -0,0 +1,13 @@
+export type Mentor = {
+    avatar_url: string
+    description: string
+    id: string
+    name: string
+    company: string
+    profession: string
+    reviewsAvgRate: number
+    reviewsCount: number
+    special: string
+    title: string
+    username: string
+}
--- src/services/fetchMentors.ts
+++ src/services/fetchMentors.ts
@@ -1,171 +1,175 @@
 import axios from 'axios'
+import {Mentor} from "@/app/types/mentor";
 
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL
 
 type ResponseData = {
-  mentors: Mentor[]
-  total: number
+    mentors: Mentor[]
+    total: number
 }
 export type SpecialVariant = 'success' | 'warning' | 'error'
-export type Mentor = {
-  avatar_url: string
-  description: string
-  id: string
-  name: string
-  company: string
-  profession: string
-  reviewsAvgRate: number
-  reviewsCount: number
-  special: string
-  title: string
-  username: string
-}
+
 
 export const fetchMentorListByCategoryValue = async (
-  category: string,
+    category: string,
 ): Promise<ResponseData> => {
-  try {
-    const filterMentorToSend = {
-      take: 8,
-      category,
-    }
-    // TODO
-    // POST ???
-    //     const response = await axios.post(
-    //       '/api/mentor/filtered-mentors',
-    //       filterMentorToSend,
-    //     )
+    try {
+        const response = await axios.get<ResponseData>('/api/landing/mentor/get-all-mentors-for-lp', {
+            params: {
+                take: 8,
+                category,
+            },
+        })
 
-    //const { mentors } = response.data
-    //return mentors;
-    const mentors = [
-      {
-        avatar_url:
-          'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
-        description:
-          'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
-        id: '1',
-        name: 'Anna Stokrotka',
-        company: 'Google',
-        profession: 'UX/UI',
-        reviewsAvgRate: 4.7,
-        reviewsCount: 10,
-        special: 'Top Mentor',
-        title: 'Java Software Developer w Google',
-        username: 'anna_stokrotka',
-      },
-      {
-        avatar_url:
-          'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
-        description:
-          'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
-        id: '2',
-        name: 'Stanisław Sójka',
-        company: 'Google',
-        profession: 'UX/UI',
-        reviewsAvgRate: 4.7,
-        reviewsCount: 1,
-        special: 'Top Mentor',
-        title: 'Java Software Developer w Google',
-        username: 'staniaslaw_sojka',
-      },
-      {
-        avatar_url:
-          'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
-        description:
-          'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
-        id: '2',
-        name: 'Stanisław Sójka',
-        company: 'Google',
-        profession: 'UX/UI',
-        reviewsAvgRate: 4.7,
-        reviewsCount: 16,
-        special: 'Top Mentor',
-        title: 'Java Software Developer w Google',
-        username: 'staniaslaw_sojka',
-      },
-      {
-        avatar_url:
-          'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
-        description:
-          'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
-        id: '2',
-        name: 'Stanisław Sójka',
-        company: 'Google',
-        profession: 'UX/UI',
-        reviewsAvgRate: 4.7,
-        reviewsCount: 122,
-        special: '',
-        title: 'Java Software Developer w Google',
-        username: 'staniaslaw_sojka',
-      },
-      {
-        avatar_url:
-          'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
-        description:
-          'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
-        id: '2',
-        name: 'Stanisław Sójka',
-        company: 'Google',
-        profession: 'UX/UI',
-        reviewsAvgRate: 4.7,
-        reviewsCount: 10,
-        special: '',
-        title: 'Java Software Developer w Google',
-        username: 'staniaslaw_sojka',
-      },
-      {
-        avatar_url:
-          'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
-        description:
-          'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
-        id: '2',
-        name: 'Stanisław Sójka',
-        company: 'Google',
-        profession: 'UX/UI',
-        reviewsAvgRate: 4.7,
-        reviewsCount: 10,
-        special: '',
-        title: 'Java Software Developer w Google',
-        username: 'staniaslaw_sojka',
-      },
-      {
-        avatar_url:
-          'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
-        description:
-          'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
-        id: '2',
-        name: 'Stanisław Sójka',
-        company: 'Google',
-        profession: 'UX/UI',
-        reviewsAvgRate: 4.7,
-        reviewsCount: 3,
-        special: '',
-        title: 'Java Software Developer w Google',
-        username: 'staniaslaw_sojka',
-      },
-      {
-        avatar_url:
-          'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
-        description:
-          'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
-        id: '2',
-        name: 'Stanisław Sójka',
-        company: 'Google',
-        profession: 'UX/UI',
-        reviewsAvgRate: 0,
-        reviewsCount: 0,
-        special: '',
-        title: 'Java Software Developer w Google',
-        username: 'staniaslaw_sojka',
-      },
-    ]
-    return {
-      mentors,
-      total: 10,
+        return response.data
+    } catch (error) {
+        console.error('Error fetching mentor list:', error)
+        throw new Error('Failed to fetch mentor list. Please try again later.')
     }
-  } catch (error) {
-    console.error('Error fetching mentors:', error)
-    throw error
-  }
 }
+
+// export const fetchMentorListByCategoryValue = async (
+//     category: string,
+// ): Promise<ResponseData> => {
+//     try {
+//         const filterMentorToSend = {
+//             take: 8,
+//             category,
+//         }
+//         const response = await axios.get(
+//             '/get-all-mentors-for-lp',
+//         )
+
+        // const {mentors} = response.data
+        //return mentors;
+        // const mentors = [
+        //     {
+        //         avatar_url:
+        //             'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
+        //         description:
+        //             'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
+        //         id: '1',
+        //         name: 'Anna Stokrotka',
+        //         company: 'Google',
+        //         profession: 'UX/UI',
+        //         reviewsAvgRate: 4.7,
+        //         reviewsCount: 10,
+        //         special: 'Top Mentor',
+        //         title: 'Java Software Developer w Google',
+        //         username: 'anna_stokrotka',
+        //     },
+        //     {
+        //         avatar_url:
+        //             'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
+        //         description:
+        //             'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
+        //         id: '2',
+        //         name: 'Stanisław Sójka',
+        //         company: 'Google',
+        //         profession: 'UX/UI',
+        //         reviewsAvgRate: 4.7,
+        //         reviewsCount: 1,
+        //         special: 'Top Mentor',
+        //         title: 'Java Software Developer w Google',
+        //         username: 'staniaslaw_sojka',
+        //     },
+        //     {
+        //         avatar_url:
+        //             'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
+        //         description:
+        //             'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
+        //         id: '2',
+        //         name: 'Stanisław Sójka',
+        //         company: 'Google',
+        //         profession: 'UX/UI',
+        //         reviewsAvgRate: 4.7,
+        //         reviewsCount: 16,
+        //         special: 'Top Mentor',
+        //         title: 'Java Software Developer w Google',
+        //         username: 'staniaslaw_sojka',
+        //     },
+        //     {
+        //         avatar_url:
+        //             'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
+        //         description:
+        //             'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
+        //         id: '2',
+        //         name: 'Stanisław Sójka',
+        //         company: 'Google',
+        //         profession: 'UX/UI',
+        //         reviewsAvgRate: 4.7,
+        //         reviewsCount: 122,
+        //         special: '',
+        //         title: 'Java Software Developer w Google',
+        //         username: 'staniaslaw_sojka',
+        //     },
+        //     {
+        //         avatar_url:
+        //             'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
+        //         description:
+        //             'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
+        //         id: '2',
+        //         name: 'Stanisław Sójka',
+        //         company: 'Google',
+        //         profession: 'UX/UI',
+        //         reviewsAvgRate: 4.7,
+        //         reviewsCount: 10,
+        //         special: '',
+        //         title: 'Java Software Developer w Google',
+        //         username: 'staniaslaw_sojka',
+        //     },
+        //     {
+        //         avatar_url:
+        //             'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
+        //         description:
+        //             'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
+        //         id: '2',
+        //         name: 'Stanisław Sójka',
+        //         company: 'Google',
+        //         profession: 'UX/UI',
+        //         reviewsAvgRate: 4.7,
+        //         reviewsCount: 10,
+        //         special: '',
+        //         title: 'Java Software Developer w Google',
+        //         username: 'staniaslaw_sojka',
+        //     },
+        //     {
+        //         avatar_url:
+        //             'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
+        //         description:
+        //             'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
+        //         id: '2',
+        //         name: 'Stanisław Sójka',
+        //         company: 'Google',
+        //         profession: 'UX/UI',
+        //         reviewsAvgRate: 4.7,
+        //         reviewsCount: 3,
+        //         special: '',
+        //         title: 'Java Software Developer w Google',
+        //         username: 'staniaslaw_sojka',
+        //     },
+        //     {
+        //         avatar_url:
+        //             'https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg',
+        //         description:
+        //             'Jestem UI Designerem z 10-letnim doświadczeniem, lorem ipsum dolor sit amet consectetur.',
+        //         id: '2',
+        //         name: 'Stanisław Sójka',
+        //         company: 'Google',
+        //         profession: 'UX/UI',
+        //         reviewsAvgRate: 0,
+        //         reviewsCount: 0,
+        //         special: '',
+        //         title: 'Java Software Developer w Google',
+        //         username: 'staniaslaw_sojka',
+        //     },
+        // ]
+        // return {
+        //     mentors,
+        //     total: 10,
+        // }
+    // } catch (error) {
+    //     console.error('Error fetching mentors:', error)
+    //     throw error
+    // }
+// }
