38c6ea1d8e92d92f2d507eac6f26aa0aabdd6ed5
maciejmaciek <hello@maciejglazer.com>
2025-02-12 22:53:51 +0100

    update

--- src/services/fetchCategories.tsx
+++ src/services/fetchCategories.tsx
@@ -10,7 +10,7 @@ type ResponseData = {
 export const fetchCategories = async (): Promise<ResponseData> => {
   try {
     const response = await axios.get(
-      'https://api.skillgu.com/api/1.0/mentors/categories',
+      '/api/1.0/mentors/categories',
     )
     return response.data
   } catch (error) {
