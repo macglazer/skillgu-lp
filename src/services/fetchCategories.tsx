635824a5f9f4e1014d7b6e9f2e9bcdab6e5c495e
maciejmaciek <hello@maciejglazer.com>
2025-02-13 14:27:30 +0100

    update

--- src/services/fetchCategories.tsx
+++ src/services/fetchCategories.tsx
@@ -7,27 +7,12 @@ type ResponseData = {
   label: string
 }[]
 
-// export const fetchCategories = async (): Promise<ResponseData> => {
-//   try {
-//     const response = await axios.get('/api/1.0/mentors/categories'.trim());
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error fetching categories:', error);
-//     throw error;
-//   }
-// }
-
 export const fetchCategories = async (): Promise<ResponseData> => {
   try {
-    const response = await axios.get('https://api.skillgu.com/api/1.0/mentors/categories', {
-      headers: {
-        Host: 'api.skillgu.com' // Wymuszenie poprawnego SNI (Server Name Indication)
-      }
-    });
+    const response = await axios.get('/api/1.0/mentors/categories'.trim());
     return response.data;
   } catch (error) {
     console.error('Error fetching categories:', error);
     throw error;
   }
 }
-
