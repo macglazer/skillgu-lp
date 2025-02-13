9a2c8017f4c69e8b8f81d136ca9e5411dfead635
maciejmaciek <hello@maciejglazer.com>
2025-02-13 08:16:44 +0100

    update

--- src/services/fetchCategories.tsx
+++ src/services/fetchCategories.tsx
@@ -8,18 +8,18 @@ type ResponseData = {
   label: string
 }[]
 
+
 const agent = new https.Agent({
-  rejectUnauthorized: false, // Tymczasowo ignoruj błędy certyfikatów
-  servername: 'api.skillgu.com', // Wymuś poprawny SNI
+  rejectUnauthorized: false,
+  servername: 'api.skillgu.com',
 });
 
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || 'https://api.skillgu.com';
+axios.defaults.httpsAgent = agent;
 
 export const fetchCategories = async (): Promise<ResponseData> => {
   try {
-    const response = await axios.get('/api/1.0/mentors/categories', {
-      httpsAgent: agent,
-    });
+    const response = await axios.get('/api/1.0/mentors/categories');
     return response.data;
   } catch (error) {
     console.error('Error fetching categories:', error);
